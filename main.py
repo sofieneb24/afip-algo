2e3a8a83ac851f85961fd013c075ff850fbf7b9f
sofieneb24 <150902000+sofieneb24@users.noreply.github.com>
2023-12-21 11:00:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from functions import *
 
 # Nom de fichier à charger
-fileName = "";
+fileName = "FIFA_World_Cup/FIFA-2022.txt"
 
 # Charge un fichier en lecture
 file = open(fileName, "r")
@@ -9,3 +9,13 @@ file = open(fileName, "r")
 # Charge les données à partir d'un fichier
 data = load_data(fileName);
 
+# Supprimer l'en-tête
+supprimer_entete(fileName)
+
+# Obtenir les trois premières lettres
+trois_lettres(fileName)
+
+# Mélanger les lignes
+melange_lignes(fileName)
+
+# Enregistrer les données dans un fichier.
